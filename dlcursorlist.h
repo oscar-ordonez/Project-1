a10f89227f3882f64046e63a99e439f49ec0be1d
Oscar Ordonez <oscar.ordonez@unitec.edu>
2015-05-17 20:51:16 -0600

    Agregando DLCursorList

---
+++ dlcursorlist.h
@@ -0,0 +1,33 @@
+#ifndef DLCURSORLIST_H
+#define DLCURSORLIST_H
+
+#include "tdalist.h"
+
+class DLCursorList : public TDAList{
+	struct Registry{
+		int prev, next;
+		Object* datum;
+	};
+
+	Registry* map;
+	int head;
+	int capacity;
+	int findNextSlot() const;
+
+public:
+	DLCursorList(int=25);
+	~DLCursorList();
+	virtual bool insert(Object*, int);
+	virtual int indexOf(Object*) const;
+  	virtual Object* get(unsigned) const;
+	virtual Object* remove(unsigned);
+	virtual int prev(int) const;
+	virtual int next(int) const;
+	virtual void clear();
+	virtual Object* first() const;
+	virtual Object* last() const;
+	virtual void print() const;
+	virtual bool isFull() const;
+};
+
+#endif
