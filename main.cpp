68668a9c54f7acf252a8dfc4b4c42d79a61cd752
Oscar Ordonez <oscar.ordonez@unitec.edu>
2015-05-18 00:21:46 -0600

    Calculando los tiempos de ejecucion

--- main.cpp
+++ main.cpp
@@ -15,109 +15,80 @@ void MetodoDLCursorList(DLCursorList*, int, int);
 void listaTDA(TDAList*);
 
 int main(int argc, char *argv[]){
-	cout << "**********MENU**********" << endl << " 1. FSArrayList" << endl << " 2. LinkedList" << endl << " 3. DlCursorList";
-	cout << endl << " 4. CursorList" << endl << " 5. VSArrayList"<< endl << " Ingrese su opcion: ";
+	cout << "**********MENU**********" << endl << " 1. FSArrayList" << endl << " 2. LinkedList" << endl << " 3. DLCursorList" << endl << " Ingrese su opcion: ";
 	int opcion;
 	cin >> opcion;
 	TDAList* list;
 	Object* data = NULL;
 	if (opcion == 1){
-		//list = new FSArrayList(5000);
-	 	//MetodoFSArrayList(list, number, position);
 	 	list = new FArrayList(5000);
 	 	listaTDA(list);
 	 } else if (opcion == 2){
-	 	//list = new LinkedList(5000);
+	 	list = new LinkedList();
+	 	listaTDA(list);
 	 	//MetodoLinkedList(list, number, position);
 	 } else if (opcion == 3){
-	 	//list = new DLCursorList(5000);
+	 	list = new DLCursorList(5000);
+	 	listaTDA(list);
 	 	//MetodoDLCursorList(list, number, position);
 	 }
 	delete list;
     return 0;
 }
+/***************************************************/
+
 void listaTDA(TDAList* list){
+	clock_t start;
+	clock_t end;
 	//insertar los 5000 mil numeros
+	cout << "AQUI SE IMPRIMI INSERT"<< endl;
 	for (int i = 0; i < 5000; ++i)
 	{
-		/* code */
+		start = clock();
 		list->insert(new Integer(i*10),i);
-	}
-
-	//imprimir
-	for (int i = 0; i < list->size(); ++i)
-	{
-		/* code */
-		cout << list->get(i)->toString() << endl;
+		end = clock()-start/CLOCKS_PER_SEC;
+		cout << end << endl;
 	}
 
 	//first
-	cout << "First = " << list->first() << endl;
+	cout << "AQUI SE IMPRIMI FIRST"<< endl;
+	for (int i = 0; i < 5000; ++i)
+	{
+		start = clock();
+		list->first();
+		end = clock()-start/CLOCKS_PER_SEC;
+		cout << end << endl;
+	}
 
 	//Last
-	cout << "Last = " << list->last() << endl;
+	cout << "AQUI SE IMPRIMI LAST"<< endl;
+	for (int i = 0; i < 5000; ++i)
+	{
+		start = clock();
+		list->last();
+		end = clock()-start/CLOCKS_PER_SEC;
+		cout << end << endl;
+	}
 	
 	//get
+	cout << "AQUI SE IMPRIMI GET"<< endl;
 	for (int i = 0; i < list->size(); ++i)
 	{
-		/* code */
+		start = clock();
 		list->get(i);
+		end = clock()-start/CLOCKS_PER_SEC;
+		cout << end << endl;
 	}
 	//remover
+	cout << "AQUI SE IMPRIMI REMOVE"<< endl;
 	Object* tmp = NULL;
 	for (int i = 0; i < list->size(); ++i)
 	{
-		/* code */
+		start = clock();
 		tmp  = list->remove(0);
+		end = clock()-start/CLOCKS_PER_SEC;
+		cout << end << endl;
 		delete tmp;
 	}
 }
-/***************************************************/
-/*void MetodoFSArrayList(FSArrayList* lista, int numero, int posicion){
-	for (int i = 0; i < 5000; ++i){
-		lista->insert(new Integer(numero),posicion);
-	}
-	clock_t start;
-	clock_t end;
-	srand(time(NULL));
-	for (int i = 0; i < 5000; ++i){
-		start = clock();
-		lista->remove(0);
-		end = clock()-start/CLOCKS_PER_SEC;
-		cout << " " << end;
-	}
-	cout << endl;
-}
-/***************************************************/
-/*void MetodoLinkedList(LinkedList* lista, int numero, int posicion){
-	for (int i = 0; i < 5000; ++i){
-		lista->insert(new Integer(numero),posicion);
-	}
-	clock_t start;
-	clock_t end;
-	srand(time(NULL));
-	for (int i = 0; i < 5000; ++i){
-		start = clock();
-		lista->remove(0);
-		end = clock()-start/CLOCKS_PER_SEC;
-		cout << " " << end;
-	}
-	cout << endl;
-}*/
-/***************************************************/
-/*void MetodoDLCursorList(DLCursorList* lista, int numero, int posicion){
-	for (int i = 0; i < 5000; ++i){
-		lista->insert(new Integer(numero),posicion);
-	}
-	clock_t start;
-	clock_t end;
-	srand(time(NULL));
-	for (int i = 0; i < 5000; ++i){
-		start = clock();
-		lista->remove(0);
-		end = clock()-start/CLOCKS_PER_SEC;
-		cout << " " << end;
-	}
-	cout << endl;
-}*/	
 /***************************************************/
