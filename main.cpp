f81b2c31d12289e541dcdc6089afea9dd304edf2
Oscar Ordonez <oscar.ordonez@unitec.edu>
2015-05-17 21:34:25 -0600

    Agregando Main

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include "tdalist.h"
+#include "vsarraylist.h" 
+#include "fsarraylist.h" 
+#include "linkedlist.h"  
+#include "dlcursorlist.h"
+#include "integer.h"
+
+using namespace std;
+
+void testTDA(TDAList*);
+
+int main(int argc, char *argv[]){
+	TDAList* list;
+	Object* data = NULL; 
+	//list = new SLinkedList();
+	//list = new FArrayList(5000);
+	list = new DLCursorList(5000);
+	int number, position;
+	for (int i = 0; i < 5000; ++i){
+		number = rand()%100;
+		position = 0+(rand()%(int)(list->size()-0+1));
+		list->insert(new Integer(number),position);
+	}
+
+	clock_t start;
+	clock_t end;
+
+	srand(time(NULL));
+
+	
+	for (int i = 0; i < 5000; ++i){
+		start = clock();
+		list->remove(0);
+		end = clock()-start/CLOCKS_PER_SEC;
+		cout << " " << end;
+	}
+	cout << endl;
+    delete list;
+    return 0;
+}
